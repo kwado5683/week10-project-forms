f78253293600fdea58bf559b5513c2cfb6d85fd7
siteatis <siteatis@users.noreply.github.com>
2025-07-02 16:51:04 +0100

    Update main.jsx
    Oops, committed just after updating references, but this file wasn't saved. Corrected manually.

--- react-Hook-Form/src/main.jsx
+++ react-Hook-Form/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import BadApp from "./form-codingch.jsx";
+import BadApp from "./BadApp.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
